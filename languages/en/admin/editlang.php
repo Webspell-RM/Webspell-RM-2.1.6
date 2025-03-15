74d9636dd2eba5bb6e6650d0b4711a4aa74645d7
Roberto <58231922+McRobert66@users.noreply.github.com>
2025-03-15 19:42:51 +0100

    Update databse.php and lang Admi IT & EN 155-03-2025

--- languages/en/admin/editlang.php
+++ languages/en/admin/editlang.php
@@ -1,24 +1,22 @@
 <?php
 
-$language_array = Array(
+$language_array = array(
 
-
-  'title'                 => 'Language File Editor',
-  'successfully_saved'    => 'File saved successfully!',
-  'error_save'            => 'Error saving file',
-  'type_lang'             => 'Language Type:',
-  'system_lang'           => 'System Languages',
-  'plugins_lang'          => 'Plugins Languages',
-  'system_file_lang'      => 'System Language File',
-  'select_file'           => 'Select File to Edit',
-  'font_size'             => 'Font Size:',
-  'edit_file'             => 'Editing File:',
-  'save'                  => 'Save Changes',
-  'system_lang_file'      => 'System Language File:',
-  'plugins_lang_file'     => 'Plugin Language File:',
-  'it_lang'               => 'Italian language',
-  'de_lang'               => 'German language',
-  'en_lang'               => 'English language',
+  'de_lang'              => 'German language',
+  'edit_file'            => 'Edit file:',
+  'en_lang'              => 'English language',
+  'error_save'           => 'Error saving the file',
+  'font_size'            => 'Font size:',
+  'it_lang'              => 'Italian language',
+  'plugins_lang'         => 'Plugin languages',
+  'plugins_lang_file'    => 'Plugin language file:',
+  'save'                 => 'Save changes',
+  'select_file'          => 'Select the file to edit',
+  'successfully_saved'   => 'File saved successfully!',
+  'system_file_lang'     => 'System language file',
+  'system_lang'          => 'System languages',
+  'system_lang_file'     => 'System language file:',
+  'title'                => 'Language file editor',
+  'type_lang'            => 'Language type:',
 
 );
-
--- languages/it/admin/editlang.php
+++ languages/it/admin/editlang.php
@@ -1,24 +1,22 @@
 <?php
 
-$language_array = Array(
+$language_array = array(
 
-
-  'title'                           => 'Editor file di Linguaggio',
-  'successfully_saved'              => 'File salvato con successo!',
-  'error_save'                      => 'Errore nel salvataggio del file',
-  'type_lang'                       => 'Tipo di Linguaggio:',
-  'system_lang'                     => 'Linguaggi di Sistema',
-  'plugins_lang'                    => 'Linguaggi dei Plugins',
-  'system_file_lang'                => 'File di Linguaggio del Sistema',
-  'select_file'                     => 'Seleziona il file da Modificare',
-  'font_size'                       => 'Dimensione del Font:',
-  'edit_file'                       => 'Modifica del File:',
-  'save'                            => 'Salva Modifiche',
-  'system_lang_file'                => 'File Linguaggio di Sistema:',
-  'plugins_lang_file'               => 'File Linguaggio dei Plugins:',
-  'it_lang'                         => 'Lingua Italiana',
-  'de_lang'                         => 'Lingua Tedesca',
-  'en_lang'                         => 'Lingua Inglese',
+  'title'                => 'Editor di file di linguaggio',
+  'successfully_saved'   => 'File salvato con successo!',
+  'error_save'           => 'Errore durante il salvataggio del file',
+  'type_lang'            => 'Tipo di lingua:',
+  'system_lang'          => 'Lingue di sistema',
+  'plugins_lang'         => 'Lingue dei plugin',
+  'system_file_lang'     => 'File di lingua del sistema',
+  'select_file'          => 'Seleziona il file da modificare',
+  'font_size'            => 'Dimensione del carattere:',
+  'edit_file'            => 'Modifica file:',
+  'save'                 => 'Salva modifiche',
+  'system_lang_file'     => 'File di lingua del sistema:',
+  'plugins_lang_file'    => 'File di lingua dei plugin:',
+  'it_lang'              => 'Lingua Italiana',
+  'de_lang'              => 'Lingua Tedesca',
+  'en_lang'              => 'Lingua Inglese',
 
 );
-
